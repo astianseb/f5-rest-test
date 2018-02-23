729cf6efed605559673e66322cd12142d4073456
astianseb <callmeseb@gmail.com>
2018-02-23 17:09:50 +0100

    First

--- baseline-start.sh
+++ baseline-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cfy blueprint upload -b f5-baseline-bp ./f5-baseline-blueprint.yaml
 cfy deployment create --skip-plugins-validation f5-baseline -b f5-baseline-bp
-cfy executions start -d f5-baseline install
+cfy executions start -vv -d f5-baseline install
